0ddbfa445fac160c984bf8239afcbfd7c708eea4
Fred Frey <f.frey6@gmail.com>
2016-12-07 14:04:51 -0500

    Futzing around with fourier transforms. Got it to *sorta* match the data

--- fourier.py
+++ fourier.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 prices = get_data(245)['prices']
 N = len(prices)
 f = np.fft.fft(prices)
+
 freqs = np.fft.fftfreq(N)
 
 t = np.arange(0, N)
@@ -17,8 +18,20 @@ recon = np.zeros(N)
 for coef, freq in zip(f, freqs):
     if coef:
         recon += coef.real * np.cos(freq * t)
-        recon += coef.imag * np.sin(freq * t)
+        recon -= coef.imag * np.sin(freq * t)
 
-plt.plot(recon)
+#Dirty hack to get them to the same height:
+ratio = prices[0] / recon[0]
+recon *= ratio
+
+reals = f.real
+imags = f.imag
+
+
+price_line, = plt.plot(prices)
+reconstructed, = plt.plot(recon)
+plt.legend([price_line, reconstructed], ['Original Prices', 'Reconstructed from DFT'])
+plt.xlabel('Time (days)')
+plt.ylabel('Price (gp)')
 plt.grid()
 plt.show()
