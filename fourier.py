1e1806161f1702cb310d9be7a7c8dbd3c8ed41a0
Fred Frey <f.frey6@gmail.com>
2016-12-08 07:57:49 -0500

    got recon from dft to fit data, but there are unexpected issues.

--- fourier.py
+++ fourier.py
@@ -6,7 +6,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-prices = get_data(245)['prices']
+prices = get_data(13190)['prices']
 N = len(prices)
 f = np.fft.fft(prices)
 
@@ -17,8 +17,8 @@ recon = np.zeros(N)
 
 for coef, freq in zip(f, freqs):
     if coef:
-        recon += coef.real * np.cos(freq * t)
-        recon -= coef.imag * np.sin(freq * t)
+        recon += coef.real * np.cos(2 * np.pi * freq * t)
+        recon -= coef.imag * np.sin(2 * np.pi * freq * t)
 
 #Dirty hack to get them to the same height:
 ratio = prices[0] / recon[0]
